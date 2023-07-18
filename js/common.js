895239b076d5ca286bc14195f5a75ede87491ce5
sojin233 <sojin233@naver.com>
2023-07-18 14:06:36 +0900

    nav js 추가

--- js/common.js
+++ js/common.js
@@ -1,3 +1,15 @@
+window.addEventListener('load', function () {
+	let nav = document.querySelector('nav');
+	let menuBtn = document.querySelector('.menu_btn');
+
+
+	// menu button click => acitve class add
+	menuBtn.addEventListener('click', function (){
+		nav.classList.toggle("active");
+	});
+
+
+});
 
 
 

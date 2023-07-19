401d4eb94319263c51e7db3f839f017ec1fab566
sojin233 <sojin233@naver.com>
2023-07-19 14:20:54 +0900

    select box 제작

--- js/common.js
+++ js/common.js
@@ -1,12 +1,50 @@
 window.addEventListener('load', function () {
-	let nav = document.querySelector('nav');
-	let menuBtn = document.querySelector('.menu_btn');
+	const nav = document.querySelector('nav');
+	const menuBtn = document.querySelector('.menu_btn');
+	const label = document.querySelector('.label');
+	const options = document.querySelectorAll('.optionItem');
+
 
 
 	// menu button click => acitve class add
 	menuBtn.addEventListener('click', function (){
 		nav.classList.toggle("active");
 	});
+
+
+  // selectbox click event 
+	const handleSelect = function(e) {
+		label.innerHTML = e.textContent;
+		label.parentNode.classList.remove('active');
+	}
+	options.forEach(function (e) {
+		e.addEventListener('click', function () {
+			handleSelect(e)
+		});
+	});
+	label.addEventListener('click', function(){
+		if(label.parentNode.classList.contains('active')) {
+			label.parentNode.classList.remove('active');
+		} else {
+			label.parentNode.classList.add('active');
+		}
+		var value = document.documentElement.lang;
+		console.log(value)
+	});
+	document.querySelector("body").addEventListener("click", function (e) {
+		if (e.target == e.currentTarget.querySelector('.label')) {
+			
+		} else {
+			label.parentNode.classList.remove('active');
+		}
+	});
+
+
+
+
+
+
+
 
 
 });
